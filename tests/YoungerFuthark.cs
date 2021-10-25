3bcfe99a456d0b3dba710ad1078fb875edd6f3be
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-25 15:56:40 +0300

    Younger Futhark: add old norse content tests

--- tests/YoungerFuthark.cs
+++ tests/YoungerFuthark.cs
@@ -18,10 +18,34 @@ namespace RiimutTest
         }
 
         [Fact]
-        public void Transforms_Younger_Futhark_To_Letters()
+        public void Transforms_Text_Content_To_Younger_Futhark()
+        {
+            // From Old Groms runestone.
+            string letters = "auk tani karþi kristna";
+            string runes = "ᛅᚢᚴ:ᛏᛅᚾᛁ:ᚴᛅᚱᚦᛁ:ᚴᚱᛁᛋᛏᚾᛅ";
+
+            string result = YoungerFuthark.LettersToRunes(letters);
+
+            Assert.Equal(runes, result);
+        }
+
+        [Fact]
+        public void Transforms_Runes_To_Letters()
         {
             string runes = "ᚠᚢᚦᚬᚱᚴᚼᚽᚾᚿᛁᛅᛆᛋᛌᛏᛐᛒᛘᛚᛦ:";
             string letters ="fuþorkhhnniaassttbmlR ";
+
+            string result = YoungerFuthark.RunesToLetters(runes);
+
+            Assert.Equal(letters, result);
+        }
+
+        [Fact]
+        public void Transforms_Rune_Content_To_Latin_Letters()
+        {
+            // From Old Groms runestone.
+            string runes = "ᛅᚢᚴ:ᛏᛅᚾᛁ:ᚴᛅᚱᚦᛁ:ᚴᚱᛁᛋᛏᚾᛅ";
+            string letters ="auk tani karþi kristna";
 
             string result = YoungerFuthark.RunesToLetters(runes);
 
