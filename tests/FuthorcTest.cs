d4094a58489a6ff507e9489c43a808608981ff0b
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-26 09:11:44 +0300

    Futhorc: add west saxon content tests

--- tests/FuthorcTest.cs
+++ tests/FuthorcTest.cs
@@ -27,5 +27,29 @@ namespace RiimutTest
 
             Assert.Equal(letters, result);
         }
+
+        [Fact]
+        public void Transforms_West_Saxon_Text_To_Futhorc_Runes()
+        {
+            // From 8th century Franks Casket, in late West Saxon.
+            string letters = "fisc.flodu.ahofonferg | enberig |";
+            string runes = "ᚠᛁᛋᚳ.ᚠᛚᚩᛞᚢ.ᚪᚻᚩᚠᚩᚾᚠᛖᚱᚷ:|:ᛖᚾᛒᛖᚱᛁᚷ:|";
+
+            string result = Futhorc.LettersToRunes(letters);
+
+            Assert.Equal(runes, result);
+        }
+
+        [Fact]
+        public void Transforms_West_Saxon_Futhorc_To_Latin_Text()
+        {
+            // From 8th century Franks Casket, in late West Saxon.
+            string runes = "ᚠᛁᛋᚳ.ᚠᛚᚩᛞᚢ.ᚪᚻᚩᚠᚩᚾᚠᛖᚱᚷ:|:ᛖᚾᛒᛖᚱᛁᚷ:|";
+            string letters = "fisc.flodu.ahofonferg | enberig |";
+
+            string result = Futhorc.RunesToLetters(runes);
+
+            Assert.Equal(letters, result);
+        }
     }
 }
