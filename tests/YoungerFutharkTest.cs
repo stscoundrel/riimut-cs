00c1c96c4b397abe5d193cb8899380c023a1c00e
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-19 19:11:32 +0300

    Younger Futhark: add long branch & short twig variants

--- tests/YoungerFutharkTest.cs
+++ tests/YoungerFutharkTest.cs
@@ -7,13 +7,37 @@ namespace RiimutTest
     public class YoungerFutharkTests
     {
         [Fact]
-        public void TransformsLettersToYoungerFuthark()
+        public void DefaultLetterTransform()
         {
             string letters = "aábcdðeéfghiíjklmnoópqrstþuúvwxyýzåäæöøǫþ";
             string runes = "ᛅᛅᛒᛋᛏᚦᛁᛁᚠᚴᚼᛁᛁᛁᚴᛚᛘᚾᚢᚢᛒᚴᚱᛋᛏᚦᚢᚢᚢᚢᛋᚢᚢᛋᚢᛅᛅᚢᚢᚢᚦ";
 
-            Dialect youngerFuthark = new YoungerFuthark();
+            YoungerFuthark youngerFuthark = new YoungerFuthark();
             string result = youngerFuthark.LettersToRunes(letters);
+
+            Assert.Equal(runes, result);
+        }
+
+        [Fact]
+        public void TransformsLettersToLongBranchRunes()
+        {
+            string letters = "aábcdðeéfghiíjklmnoópqrstþuúvwxyýzåäæöøǫþ";
+            string runes = "ᛅᛅᛒᛋᛏᚦᛁᛁᚠᚴᚼᛁᛁᛁᚴᛚᛘᚾᚢᚢᛒᚴᚱᛋᛏᚦᚢᚢᚢᚢᛋᚢᚢᛋᚢᛅᛅᚢᚢᚢᚦ";
+
+            YoungerFuthark youngerFuthark = new YoungerFuthark();
+            string result = youngerFuthark.LettersToLongBranchRunes(letters);
+
+            Assert.Equal(runes, result);
+        }
+
+        [Fact]
+        public void TransformsLettersToShortTwigRunes()
+        {
+            string letters = "aábcdðeéfghiíjklmnoópqrstþuúvwxyýzåäæöøǫþ";
+            string runes = "ᛆᛆᛒᛌᛐᚦᛁᛁᚠᚴᚽᛁᛁᛁᚴᛚᛘᚿᚢᚢᛒᚴᚱᛌᛐᚦᚢᚢᚢᚢᛌᚢᚢᛌᚢᛆᛆᚢᚢᚢᚦ";
+
+            YoungerFuthark youngerFuthark = new YoungerFuthark();
+            string result = youngerFuthark.LettersToShortTwigRunes(letters);
 
             Assert.Equal(runes, result);
         }
@@ -25,7 +49,7 @@ namespace RiimutTest
             string letters = "auk tani karþi kristna";
             string runes = "ᛅᚢᚴ:ᛏᛅᚾᛁ:ᚴᛅᚱᚦᛁ:ᚴᚱᛁᛋᛏᚾᛅ";
 
-            Dialect youngerFuthark = new YoungerFuthark();
+            YoungerFuthark youngerFuthark = new YoungerFuthark();
             string result = youngerFuthark.LettersToRunes(letters);
 
             Assert.Equal(runes, result);
@@ -34,13 +58,17 @@ namespace RiimutTest
         [Fact]
         public void TransformsRunesToLetters()
         {
-            string runes = "ᚠᚢᚦᚬᚱᚴᚼᚽᚾᚿᛁᛅᛆᛋᛌᛏᛐᛒᛘᛚᛦ:";
+            // Both long branch & short twig should produce same letters.
+            string longBranchRunes = "ᚠᚢᚦᚬᚱᚴᚼᚽᚾᚿᛁᛅᛆᛋᛌᛏᛐᛒᛘᛚᛦ:";
+            string shortTwigRunes = "ᚠᚢᚦᚬᚱᚴᚽᚽᚿᚿᛁᛆᛆᛌᛌᛐᛐᛒᛘᛚᛦ:";
             string letters = "fuþorkhhnniaassttbmlR ";
 
-            Dialect youngerFuthark = new YoungerFuthark();
-            string result = youngerFuthark.RunesToLetters(runes);
+            YoungerFuthark youngerFuthark = new YoungerFuthark();
+            string result1 = youngerFuthark.RunesToLetters(longBranchRunes);
+            string result2 = youngerFuthark.RunesToLetters(shortTwigRunes);
 
-            Assert.Equal(letters, result);
+            Assert.Equal(letters, result1);
+            Assert.Equal(letters, result2);
         }
 
         [Fact]
@@ -50,10 +78,55 @@ namespace RiimutTest
             string runes = "ᛅᚢᚴ:ᛏᛅᚾᛁ:ᚴᛅᚱᚦᛁ:ᚴᚱᛁᛋᛏᚾᛅ";
             string letters = "auk tani karþi kristna";
 
-            Dialect youngerFuthark = new YoungerFuthark();
+            YoungerFuthark youngerFuthark = new YoungerFuthark();
             string result = youngerFuthark.RunesToLetters(runes);
 
             Assert.Equal(letters, result);
+        }
+
+        [Fact]
+        public void StyleVariantSettersChangeRuneset()
+        {
+            string letters = "aábcdðeéfghiíjklmnoópqrstþuúvwxyýzåäæöøǫþ";
+            string expectedLongBranchRunes = "ᛅᛅᛒᛋᛏᚦᛁᛁᚠᚴᚼᛁᛁᛁᚴᛚᛘᚾᚢᚢᛒᚴᚱᛋᛏᚦᚢᚢᚢᚢᛋᚢᚢᛋᚢᛅᛅᚢᚢᚢᚦ";
+            string expectedShortTwigRunes = "ᛆᛆᛒᛌᛐᚦᛁᛁᚠᚴᚽᛁᛁᛁᚴᛚᛘᚿᚢᚢᛒᚴᚱᛌᛐᚦᚢᚢᚢᚢᛌᚢᚢᛌᚢᛆᛆᚢᚢᚢᚦ";
+
+            YoungerFuthark youngerFuthark = new YoungerFuthark();
+
+            // Expected to use long branch as default.
+            string longBranchResult = youngerFuthark.LettersToRunes(letters);
+
+            // Change to short twig.
+            youngerFuthark.EnableShortTwig();
+            string shortTwigResult = youngerFuthark.LettersToRunes(letters);
+
+            // Switch back to long branch
+            youngerFuthark.EnableLongBranch();
+            string secondLongBranchResult = youngerFuthark.LettersToRunes(letters);
+
+            Assert.Equal(expectedLongBranchRunes, longBranchResult);
+            Assert.Equal(expectedShortTwigRunes, shortTwigResult);
+            Assert.Equal(expectedLongBranchRunes, secondLongBranchResult);
+        }
+
+        [Fact]
+        public void ConstructorsDefineRuneSetToUse()
+        {
+            string letters = "aábcdðeéfghiíjklmnoópqrstþuúvwxyýzåäæöøǫþ";
+            string expectedLongBranchRunes = "ᛅᛅᛒᛋᛏᚦᛁᛁᚠᚴᚼᛁᛁᛁᚴᛚᛘᚾᚢᚢᛒᚴᚱᛋᛏᚦᚢᚢᚢᚢᛋᚢᚢᛋᚢᛅᛅᚢᚢᚢᚦ";
+            string expectedShortTwigRunes = "ᛆᛆᛒᛌᛐᚦᛁᛁᚠᚴᚽᛁᛁᛁᚴᛚᛘᚿᚢᚢᛒᚴᚱᛌᛐᚦᚢᚢᚢᚢᛌᚢᚢᛌᚢᛆᛆᚢᚢᚢᚦ";
+
+            YoungerFuthark youngerFutharkDefault = new YoungerFuthark();
+            YoungerFuthark youngerFutharkLongBranch = new YoungerFuthark(YoungerFuthark.Variant.LongBranch);
+            YoungerFuthark youngerFutharkShortTwig = new YoungerFuthark(YoungerFuthark.Variant.ShortTwig);
+
+            string defaultResult = youngerFutharkDefault.LettersToRunes(letters);
+            string shortTwigResult = youngerFutharkShortTwig.LettersToRunes(letters);
+            string longBranchResult = youngerFutharkLongBranch.LettersToRunes(letters);
+
+            Assert.Equal(expectedLongBranchRunes, defaultResult);
+            Assert.Equal(expectedShortTwigRunes, shortTwigResult);
+            Assert.Equal(expectedLongBranchRunes, longBranchResult);
         }
     }
 }
